ead72c812f64f2bf62d25f3db73af9bc47a7734b
FlaviaVel284 <v_flavia99@yahoo.com>
2021-12-03 17:59:37 +0200

    Login and Register basic functionality , only with email & password

--- app/src/main/java/com/example/bonscan/RegisterActivity.java
+++ app/src/main/java/com/example/bonscan/RegisterActivity.java
@@ -1,14 +1,70 @@
 package com.example.bonscan;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        mAuth = FirebaseAuth.getInstance();
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+    }
+
+    public void registerButtonOnClick(View view) {
+        TextInputLayout emailInputLayout = findViewById(R.id.emailInput);
+        String email = String.valueOf(emailInputLayout.getEditText().getText());
+
+        TextInputLayout passwordInputLayout = findViewById(R.id.passwordInput);
+        String password = String.valueOf(passwordInputLayout.getEditText().getText());
+
+        TextInputLayout password2InputLayout = findViewById(R.id.password2Input);
+        String password2 = String.valueOf(password2InputLayout.getEditText().getText());
+
+        if(password.equals(password2)) {
+            createAccount(email, password);
+        }
+    }
+
+    private void createAccount(String email, String password){
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = mAuth.getCurrentUser();
+                        } else {
+                            Log.w( "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(RegisterActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+    }
+
+    //TODO: password min 6 characters error!
+    //TODO: empty form error!
 }
