e695060aafd4dfe4fc5cc329820c2012eafdb02d
FlaviaVel284 <v_flavia99@yahoo.com>
2021-12-03 13:58:21 +0200

    Change activities after delay and after text click

--- app/src/main/java/com/example/bonscan/LoginActivity.java
+++ app/src/main/java/com/example/bonscan/LoginActivity.java
@@ -2,7 +2,10 @@ package com.example.bonscan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        TextView registerText = findViewById(R.id.notRegistered);
+    }
+
+    public void onClickRegister(View view) {
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+    }
+
+    public void onClickForgotPass(View view) {
     }
 }
